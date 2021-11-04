333c9fca5c04365955bef6e9de84d95547e58283
phmeyreles <principalphmeyreles3@gmail.com>
2021-11-04 16:50:04 -0300

    Functional programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,16 +8,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		List<Integer> myInts = Arrays.asList(5, 2, 10);
-		printList(myInts);
-	}
-
-	/*
-	 * However, you cannot add data to a wildcard type collection, because the
-	 * compiler doesn't know the specific type from which the list was instantiated.
-	 */
-	public static void printList(List<?> list) {
-		for (Object obj : list) {
-			System.out.println(obj);
-		}
+		myInts.forEach(System.out::println);
 	}
 }
+
